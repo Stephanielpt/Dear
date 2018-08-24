7a41bef7ab5fd4e450088931bb802126b0cf1596
Stephanielpt <stephanielampotang@gmail.com>
2018-08-23 19:24:23 -0700

    Initial Commit

---
+++ dear/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dear
+//
+//  Created by Stephanie Lampotang on 8/23/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dear/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dear
+//
+//  Created by Stephanie Lampotang on 8/23/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
